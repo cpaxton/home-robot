86c88a5c4c0c0ab1acf85c774e3cae5b087209b0
yas777 <yko207@nyu.edu>
2024-04-16 22:16:57 -0700

    lidar Node Updated

--- src/home_robot_hw/home_robot_hw/ros/lidar.py
+++ src/home_robot_hw/home_robot_hw/ros/lidar.py
@@ -60,7 +60,7 @@ class RosLidar(object):
         """Wait for image. Needs to be sort of slow, in order to make sure we give it time
         to update the image in the backend."""
         rate = rospy.Rate(5)
-        while not rospy.is_shutdown():
+        while rclpy.ok():
             with self._lock:
                 if self._points is not None:
                     break
