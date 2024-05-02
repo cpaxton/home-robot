d10bad1f30160bb3c59dcaa748207528873d0e1c
Chris Paxton <cpaxton@hello-robot.com>
2024-05-02 16:00:21 -0400

    update goal code

--- src/robot_hw_python/robot_hw_python/remote/api.py
+++ src/robot_hw_python/robot_hw_python/remote/api.py
@@ -6,6 +6,7 @@ from typing import Dict, Iterable, List, Optional
 
 import numpy as np
 import torch
+
 from home_robot.core.interfaces import Observations
 from home_robot.core.robot import ControlMode, RobotClient
 from home_robot.motion.robot import RobotModel
@@ -76,6 +77,10 @@ class StretchClient(RobotClient):
     @property
     def model(self):
         return self._robot_model
+
+    def at_goal(self) -> bool:
+        """Returns true if we have up to date head info and are at goal position"""
+        return self.nav.at_goal()
 
     # Mode interfaces
 
@@ -161,20 +166,26 @@ class StretchClient(RobotClient):
         """Move the arm and head into manip mode posture: gripper down, head facing the gripper."""
         self.switch_to_manipulation_mode()
         self.head.look_at_ee(blocking=False)
-        self.manip.goto_joint_positions(self.manip._extract_joint_pos(STRETCH_PREGRASP_Q))
+        self.manip.goto_joint_positions(
+            self.manip._extract_joint_pos(STRETCH_PREGRASP_Q)
+        )
         print("- Robot switched to manipulation mode.")
 
     def move_to_demo_pregrasp_posture(self):
         """Move the arm and head into pre-demo posture: gripper straight, arm way down, head facing the gripper."""
         self.switch_to_manipulation_mode()
         self.head.look_at_ee(blocking=False)
-        self.manip.goto_joint_positions(self.manip._extract_joint_pos(STRETCH_DEMO_PREGRASP_Q))
+        self.manip.goto_joint_positions(
+            self.manip._extract_joint_pos(STRETCH_DEMO_PREGRASP_Q)
+        )
 
     def move_to_pre_demo_posture(self):
         """Move the arm and head into pre-demo posture: gripper straight, arm way down, head facing the gripper."""
         self.switch_to_manipulation_mode()
         self.head.look_at_ee(blocking=False)
-        self.manip.goto_joint_positions(self.manip._extract_joint_pos(STRETCH_PREDEMO_Q))
+        self.manip.goto_joint_positions(
+            self.manip._extract_joint_pos(STRETCH_PREDEMO_Q)
+        )
 
     def move_to_nav_posture(self):
         """Move the arm and head into nav mode. The head will be looking front."""
@@ -182,7 +193,9 @@ class StretchClient(RobotClient):
         # First retract the robot's joints
         self.switch_to_manipulation_mode()
         self.head.look_front(blocking=False)
-        self.manip.goto_joint_positions(self.manip._extract_joint_pos(STRETCH_NAVIGATION_Q))
+        self.manip.goto_joint_positions(
+            self.manip._extract_joint_pos(STRETCH_NAVIGATION_Q)
+        )
         self.switch_to_navigation_mode()
         print("- Robot switched to navigation mode.")
 
@@ -190,7 +203,9 @@ class StretchClient(RobotClient):
         """Move the arm to nav mode, head to nav mode with PREGRASP's tilt. The head will be looking front."""
         self.switch_to_manipulation_mode()
         self.head.look_front(blocking=False)
-        self.manip.goto_joint_positions(self.manip._extract_joint_pos(STRETCH_POSTNAV_Q))
+        self.manip.goto_joint_positions(
+            self.manip._extract_joint_pos(STRETCH_POSTNAV_Q)
+        )
         self.switch_to_navigation_mode()
 
     def get_base_pose(self) -> np.ndarray:
@@ -257,8 +272,9 @@ class StretchClient(RobotClient):
         return torch.from_numpy(self.head._ros_client.rgb_cam.K).float()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import rclpy
+
     rclpy.init()
     client = StretchClient()
     breakpoint()
